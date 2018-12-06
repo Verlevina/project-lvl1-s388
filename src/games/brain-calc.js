806d09db74a9e4921ed5140ff75fed6839fd1e53
Levitina Veronika <levitinava@gmail.com>
2018-12-06 19:15:57 +0300

    add ranndom sign to brain-calc

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,16 +1,38 @@
-import { cons } from 'hexlet-pairs';
+import { cons, car, cdr } from 'hexlet-pairs';
 import gameTemplate from '../game-template';
 import getRandomNumbers from '../util';
 
 const MIN = 1;
-const MAX = 100;
+const MAX = 10;
+const countOperation = 4;
+const startCountOperation = 1;
 const calcGameDescription = 'What is the result of the expression?';
+const getSign = (num)=> {
+  let pair;
+    switch(num) {
+      case 1:
+        pair = cons('+', (a, b)=> a + b);
+        break;
+      case 2:
+        pair = cons('-', (a, b)=> a - b);
+        break;
+      case 3:
+        pair = cons('*', (a, b)=> a * b);
+        break;
+      case 4:
+        pair = cons('/', (a, b)=> a / b);
+        break;
+    }
+    return pair;
+};
 
 const questionPairGenerate = () => {
   const firstNumber = getRandomNumbers(MIN, MAX);
   const secondNumber = getRandomNumbers(MIN, MAX);
-  const question = ` ${firstNumber} + ${secondNumber}`;
-  const trueAnswer = (firstNumber + secondNumber).toString();
+  const randomNum = getRandomNumbers(startCountOperation, countOperation);
+  const signPair = getSign(randomNum);
+  const question = ` ${firstNumber} ${car(signPair)} ${secondNumber}`;
+  const trueAnswer = ((cdr(signPair))(firstNumber, secondNumber)).toString();
   return cons(question, trueAnswer);
 };
 
