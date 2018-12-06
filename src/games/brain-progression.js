5f7a1bdcbe6bf610405e2c91108c9993661db18f
Levitina Veronika <levitinava@gmail.com>
2018-12-06 17:14:32 +0300

    fix

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -8,7 +8,7 @@ const progressionLength = 10;
 const progressionGameDescription = 'What number is missing in the progression?';
 
 
-const getQuestion = (progressionValue, questionPosition, step, answer, count = 0, question = '') => {
+const getPair = (progressionValue, questionPosition, step, answer, count = 0, question = '') => {
   let newAnswer = answer;
   let newQuestion = question;
   let newProgressionValue = progressionValue;
@@ -30,7 +30,7 @@ const questionPairGenerate = () => {
   const startProgression = getRandomNumbers(MIN, MAX);
   const questionPosition = getRandomNumbers(0, progressionLength);
   const step = getRandomNumbers(MIN, MAX);
-  return getQuestion(startProgression, questionPosition, step);
+  return getPair(startProgression, questionPosition, step);
 };
 
 export default () => gameTemplate(questionPairGenerate, progressionGameDescription);
