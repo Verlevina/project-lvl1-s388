65406962c8615b514d3c795946be78aaa786e4e7
Levitina Veronika <levitinava@gmail.com>
2018-12-07 14:03:42 +0300

    переписан calc

--- src/games/calc.js
+++ src/games/calc.js
@@ -7,29 +7,25 @@ const MAX = 100;
 const countOperation = 3;
 const startCountOperation = 0;
 const description = 'What is the result of the expression?';
-const getSign = (signIndex, firstNumber, secondNumber) => {
-  let pairQuestion;
-  switch (signIndex) {
-    case 0:
-      pairQuestion = cons(`${firstNumber}+${secondNumber}`, (firstNumber + secondNumber).toString());
-      break;
-    case 1:
-      pairQuestion = cons(`${firstNumber}-${secondNumber}`, (firstNumber - secondNumber).toString());
-      break;
-    case 2:
-      pairQuestion = cons(`${firstNumber}*${secondNumber}`, (firstNumber * secondNumber).toString());
-      break;
-    default:
-  }
-  return pairQuestion;
-};
 
 const questionPairGenerate = () => {
   const firstNumber = getRandomNumber(MIN, MAX);
   const secondNumber = getRandomNumber(MIN, MAX);
   const signIndex = getRandomNumber(startCountOperation, countOperation);
-  const signPair = getSign(signIndex, firstNumber, secondNumber);
-  return signPair();
+  let pairQuestion;
+  switch (signIndex) {
+    case 0:
+      pairQuestion = cons(`${firstNumber} + ${secondNumber}`, (firstNumber + secondNumber).toString());
+      break;
+    case 1:
+      pairQuestion = cons(`${firstNumber} - ${secondNumber}`, (firstNumber - secondNumber).toString());
+      break;
+    case 2:
+      pairQuestion = cons(`${firstNumber} * ${secondNumber}`, (firstNumber * secondNumber).toString());
+      break;
+    default:
+  }
+  return pairQuestion;
 };
 
 export default () => gameTemplate(questionPairGenerate, description);
